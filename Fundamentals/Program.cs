96c1cd76d8e65aada7205575ac00bbc192f58e77
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-04-04 18:57:04 -0500

    This is the start, were beginning again..

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -11,11 +11,10 @@ namespace Fundamentals
         static void Main(string[] args)
         {
             AudiCar myAudi = new AudiCar();
-            // Calling different methods from the class itself
-
-
-            // myAudi._location = "Germany";
-            // myAudi._model = "RS7";
+            
+             // Calling different methods from the class itself
+             myAudi._location = "Germany";
+             myAudi._model = "RS7";
 
             Console.WriteLine("Welcome to your new and improved Audi, sent from Germany");
         }
