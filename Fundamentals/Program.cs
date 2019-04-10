3195d6bad92054aaeeab253154a94953f2dffd33
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-04-09 23:10:24 -0500

    test

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -8,16 +8,31 @@ namespace Fundamentals
 {
     class AudiCar
     {
+        public AudiCar(int loc, int make, int model)
 
         // Learned to create strings that can be called through the console, must be outside of the Main func. to be properly called
 
         // Private strings that can only be used in this class
         private string location;
+        private string make;
         private string model;
 
+        
         // Used GET and SET on both location and model to get both of them and assign them to a value assigned
-        public string Location { get => location; set => location = value; }
-        public string Model { get => model; set => model = value; }
+        public string Location
+        {
+            get => location; set => location = value;
+        }
+
+        public string Model
+        {
+            get => model; set => model = value;
+        }
+
+        public string Make 
+        {
+            get => make; set => make = value;
+        }
 
         static void Main(string[] args)
         {
@@ -25,22 +40,13 @@ namespace Fundamentals
             // New AudiCar object
             AudiCar myAudi = new AudiCar();
             
-             // Calling different methods from the class itself
-             myAudi.Location = "Germany";
-             myAudi.Model = "RS7";
-
-            // Calling the location
-            Console.WriteLine(myAudi.Location);
-            Console.WriteLine(myAudi.Model);
+            Console.WriteLine("Welcome to your new and improved Audi, sent and made from Germany!");
 
         }
 
         // Following code are all public classes
 
-        public void CreateCar()
-        {
-            Console.WriteLine("Welcome to your new and improved Audi, sent from Germany");
-        }
+
 
     }
 }
