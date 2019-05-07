7e55a62ce5a6a0379bfbb64c94b18239fe8ef77f
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-06 19:16:25 -0500

    Date time figured out and made for prac.

---
+++ Fundamentals/Date.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    class Date
+    {
+        public static void DateTiming()
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: {0}", now.Hour);
+            Console.WriteLine("Hour: {0}", now.Minute);
+
+        }
+    }
+}
